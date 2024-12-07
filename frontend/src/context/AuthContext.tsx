527d434dc398590af4f84678452d88e912458d86
Ali Khalifa <Alykhalifa1998@gmail.com>
2024-12-07 15:38:22 +0200

    Added auth context and axios configurations

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import axiosInstance from "../utils/axios";
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await axiosInstance.get("/auth/validate-token");
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    validateToken();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
