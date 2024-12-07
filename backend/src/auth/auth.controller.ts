e6f7a33e76c6571d84372349cf952412b8ac0030
Ali Khalifa <Alykhalifa1998@gmail.com>
2024-12-07 15:40:12 +0200

    backend: added cookie token validation endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Logger,
   Post,
+  Req,
+  Res,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDTO } from './dto/signup.dto';
 import { SigninDTO } from './dto/signin.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
@@ -21,27 +26,27 @@ export class AuthController {
   @ApiOperation({ summary: 'Sign up a new user' })
   @HttpCode(HttpStatus.CREATED)
   @ApiResponse({ status: 201, description: 'User created successfully' })
-  async signup(@Body() body: SignupDTO) {
+  async signup(@Body() body: SignupDTO, @Res() res: Response) {
     try {
       this.logger.log(
         `Received signup request with body: ${JSON.stringify(body)}`,
       );
       const jwtToken = await this.authService.signup(body);
       this.logger.log(`User successfully created with token: ${jwtToken}`);
-
-      return {
-        message: 'User created successfully',
-        data: {
-          jwtToken,
-        },
-      };
+      res
+        .cookie('authToken', jwtToken, {
+          httpOnly: true,
+          sameSite: 'strict',
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .json({ message: 'User created successfully' });
     } catch (error) {
       throw error;
     }
   }
   @Post('signin')
   @HttpCode(200)
-  async signin(@Body() body: SigninDTO) {
+  async signin(@Body() body: SigninDTO, @Res() res: Response) {
     try {
       this.logger.log(`Processing signin request for email: ${body.email}`);
 
@@ -57,18 +62,45 @@ export class AuthController {
         );
       }
 
-      return {
-        message: 'User signed in successfully',
-        data: {
-          jwtToken,
-        },
-      };
+      res
+        .cookie('authToken', jwtToken, {
+          httpOnly: true,
+          sameSite: 'strict',
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .json({ message: 'User signed in successfully' });
     } catch (error) {
       this.logger.error(
         `Error during signin for email ${body.email}: ${error.message}`,
         error.stack,
       );
       throw error;
+    }
+  }
+
+  @Get('validate-token')
+  async validateToken(@Req() req: Request, @Res() res: Response) {
+    try {
+      const token =
+        req.cookies['authToken'] || req.headers['authorization']?.split(' ')[1];
+
+      if (!token) {
+        throw new UnauthorizedException('Token is required');
+      }
+
+      await this.authService.validateToken(token);
+      res.status(200).send({
+        message: 'Valid token',
+      });
+      return;
+    } catch (error) {
+      res.clearCookie('authToken', {
+        httpOnly: true,
+        sameSite: 'strict',
+      });
+      res.status(401).send({
+        message: 'Invalid token',
+      });
     }
   }
 }
